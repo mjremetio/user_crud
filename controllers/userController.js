93850db170bcb147b71ae307cb2b8755178def21
mjremetio <mj.remetio001@gmail.com>
2020-08-14 20:11:07 +0800

    add some functionalities
    add some functionalities

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,9 @@ const express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json('sample test');
+    res.render("user/addOrEdit", {
+        viewTitle: "Insert User"
+    });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 require('./models/db');
 
 const express = require('express');
-
-const userControler = require('./controllers/userController');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const userController = require('./controllers/userController');
 
 var app = express();
+
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
+app.set('view engine', 'hbs');
 
 app.listen(3000, () => {
     console.log('Express JS started on port:3000');
