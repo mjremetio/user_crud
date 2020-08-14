17b9a5d37f26201ca9d0a18a834cf36d7e456754
mjremetio <mj.remetio001@gmail.com>
2020-08-14 21:11:57 +0800

    added functionalities
    added functionalities

--- models/db.js
+++ models/db.js
@@ -1,38 +1,7 @@
-const MongoClient = require("mongodb").MongoClient;
-const ObjectID = require('mongodb').ObjectID;
-const dbname = "UsersDB";
-const url = "mongodb://localhost:27017";
-const mongoOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-};
+const mongoose = require('mongoose');
 
-const state = {
-    db: null
-};
-
-const connect = (cb) => {
-    if (state.db) {
-        cb();
-    } else {
-        MongoClient.connect(url, mongoOptions, (err, client) => {
-            if (err) {
-                cb(err);
-            } else {
-                state.db = client.db(dbname);
-                cb();
-            }
-        });
-    }
-}
-
-const getPrimaryKey = (_id) => {
-    return ObjectID(_id);
-}
-
-const getDB = () => {
-    return state.db;
-}
-module.exports = { getDB, connect, getPrimaryKey };
+mongoose.connect('mongodb://localhost:27017/UsersDB', { useNewUrlParser: true }, (err) => {
+    if (!err) { console.log('MongoDB Connection Succeeded.') } else { console.log('Error in DB connection : ' + err) }
+});
 
 require('./user.model');
--- models/user.model.js
+++ models/user.model.js
@@ -15,4 +15,10 @@ var userSchema = new mongoose.Schema({
     }
 });
 
+
+userSchema.path('email').validate((val) => {
+    emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(val);
+}, 'Invalid e-mail.');
+
 mongoose.model('User', userSchema);
